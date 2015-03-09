da08d8f9c7282f7de30482af8315170d1c561586
morsalins <mim120291@gmail.com>
2015-03-10 01:48:19 +0600

    Created

---
+++ 11152 - Colourful Flowers.cpp
@@ -0,0 +1,89 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+#include <functional>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <set>
+#include <stack>
+#include <queue>
+#include <cctype>
+#include <math.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define I64 long long int
+#define INF 0x7f7f7f7f
+#define SIZE 1000
+#define MAX 2000
+#define EPS 1e-9
+#define PI 3.141592653589793
+
+#define PII pair <int, int>
+#define PLL pair <I64, I64>
+#define PDD pair <double, double>
+#define PSI pair <string, int>
+#define PIS pair <int, string>
+#define PSS pair <string string>
+
+#define MII map <int, int>
+#define MLL map <I64, I64>
+#define MDD map <double, double>
+#define MSI map <string, int>
+#define MIS map <int, string>
+#define MSS map <string string>
+
+#define VI vector <int>
+#define VS vector <string>
+#define QI queue <int>
+#define QS queue <string>
+#define SI stack <int>
+#define SS stack <string>
+
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+
+#define IT iterator
+#define ff first
+#define ss second
+
+#define SET(a, b) memset( a, b, sizeof (a) )
+#define READ(f) freopen(f, "r", stdin)
+#define WRITE(f) freopen(f, "w", stdout)
+
+#define IAMHERE cout << "YES\n";
+#define DEBUG(a, b) cout << a << ": " << b << "\n";
+
+int main()
+{
+    //READ("in.txt");
+    //WRITE("out.txt");
+    int Case=1;
+    double a, b, c;
+
+    while (scanf("%lf %lf %lf", &a, &b, &c) == 3) {
+        double s = (a+b+c) / 2.00;
+        double triangleArea = sqrt(s * (s-a) * (s-b) * (s-c));
+
+        double inscribedCircleRadius = triangleArea / s;
+        double inscribedCircleArea = inscribedCircleRadius * inscribedCircleRadius * PI;
+
+        double exCircleRadius = (a*b*c) / (4.00*triangleArea);
+        double exCircleArea = exCircleRadius * exCircleRadius * PI;
+
+        double rose = inscribedCircleArea;
+        double violet = triangleArea - inscribedCircleArea;
+        double sunflower = exCircleArea - triangleArea;
+        printf("%0.4lf %0.4lf %0.4lf\n", sunflower, violet, rose);
+    }
+    return 0;
+}
+
